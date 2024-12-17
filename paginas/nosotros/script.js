a6978593af86020c1715d6326e00af55e4568aa2
Jesús Emanuel Dupre <dupre.dev@gmail.com>
2024-12-17 01:58:03 -0300

    Update and Add Pages

---
+++ paginas/nosotros/script.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("main-header");
+const logo = document.getElementById("main-logo");
+
+// Rutas corregidas
+const defaultLogo = "./src/LOGO-HORIZONTAL-PNG-A-COLOR.png";
+const scrolledLogo = "./src/LOGO-SOLO-A-COLOR.png";
+
+// Función para manejar el scroll
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
